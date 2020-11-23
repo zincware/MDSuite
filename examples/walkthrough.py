4970a4ba5905f1a7efd4c88a314f4dd665bed87e
SamTov <tovey.samuel@gmail.com>
2020-11-23 15:46:50 +0100

    Convert particle uwnrapping to tensor operation
    Former-commit-id: 85143da7ff3e487380f39cd54aa1eff58c63b7b2

--- examples/walkthrough.py
+++ examples/walkthrough.py
@@ -3,14 +3,17 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import mdsuite as mds # Cool name for professionalism purposes
 
-NaCl_1400K = mds.Experiment(analysis_name="Ar", timestep=0.002, temperature=80.0, units='metal')
+NaCl_1400K = mds.Experiment(analysis_name="NaCl", timestep=0.002, temperature=80.0, units='metal')
 
-NaCl_1400K.add_data(trajectory_file='trajectory_files/NaCl_1400K.dump')
+#NaCl_1400K.add_data(trajectory_file='trajectory_files/NaCl_1400K.dump')
 
-NaCl_1400K.unwrap_coordinates()
+#aCl_1400K.unwrap_coordinates()
 
 #NaCl_1400K.print_class_attributes()
 
-#NaCl_1400K.einstein_diffusion_coefficients(plot=True, data_range=300)
+NaCl_1400K.einstein_diffusion_coefficients(plot=True, data_range=150)
+#NaCl_1400K.green_kubo_diffusion_coefficients(plot=True, data_range=50)
 
-#NaCl_1400K.write_xyz(property="Unwrapped_Positions")
+print(NaCl_1400K.diffusion_coefficients)
+
+NaCl_1400K.write_xyz(property="Unwrapped_Positions")
