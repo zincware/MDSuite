df4e8b3c63c14f2ec84030b67a2f403b96bfac92
Marco Brueckner <marco.brueckner97@web.de>
2020-12-21 09:31:08 +0100

    added to strucfactor
    Former-commit-id: 8c46206124ada40290741358be31d470e4416cce

--- mdsuite/experiment/experiment.py
+++ mdsuite/experiment/experiment.py
@@ -610,8 +610,8 @@ class Experiment(methods.ProjectMethods):
     # TODO def green_kubo_viscosity(self):
 
     # TODO def structure_factor(self):
-    def structure_factor(self):
-        struc_obj = structure_factor.StructureFactor(self)
-        struc_obj.run_analysis()
+    def structure_factor(self, Q, rho):
+        calculation_strfac = structure_factor.StructureFactor(self, Q, rho)
+        calculation_strfac.run_analysis()
 
     # TODO def angular_distribution_function(self):
