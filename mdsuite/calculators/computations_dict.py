4207545b08dab458c261f72782edc4d42c2d56a2
Fran <fratorhe@vki.ac.be>
2021-02-05 15:41:49 +0100

    implemented viscosity for trajectory files

--- mdsuite/calculators/computations_dict.py
+++ mdsuite/calculators/computations_dict.py
@@ -5,19 +5,20 @@ Summary
 -------
 """
 
+from mdsuite.calculators.coordination_number_calculation import CoordinationNumbers
 from mdsuite.calculators.einstein_diffusion_coefficients import EinsteinDiffusionCoefficients
+from mdsuite.calculators.einstein_helfand_ionic_conductivity import EinsteinHelfandIonicConductivity
+from mdsuite.calculators.einstein_helfand_thermal_conductivity import EinsteinHelfandThermalConductivity
+from mdsuite.calculators.flux_thermal import GreenKuboThermalConductivityFlux
+from mdsuite.calculators.flux_viscosity import GreenKuboViscosityFlux
 from mdsuite.calculators.green_kubo_diffusion_coefficients import GreenKuboDiffusionCoefficients
 from mdsuite.calculators.green_kubo_ionic_conductivity import GreenKuboIonicConductivity
-from mdsuite.calculators.einstein_helfand_ionic_conductivity import EinsteinHelfandIonicConductivity
-from mdsuite.calculators.radial_distribution_function import RadialDistributionFunction
-from mdsuite.calculators.coordination_number_calculation import CoordinationNumbers
-from mdsuite.calculators.potential_of_mean_force import PotentialOfMeanForce
-from mdsuite.calculators.kirkwood_buff_integrals import KirkwoodBuffIntegral
 from mdsuite.calculators.green_kubo_thermal_conductivity import GreenKuboThermalConductivity
-from mdsuite.calculators.flux_thermal import GreenKuboThermalConductivityFlux
+from mdsuite.calculators.green_kubo_viscosity import GreenKuboViscosity
+from mdsuite.calculators.kirkwood_buff_integrals import KirkwoodBuffIntegral
+from mdsuite.calculators.potential_of_mean_force import PotentialOfMeanForce
+from mdsuite.calculators.radial_distribution_function import RadialDistributionFunction
 from mdsuite.calculators.structure_factor import StructureFactor
-from mdsuite.calculators.einstein_helfand_thermal_conductivity import EinsteinHelfandThermalConductivity
-from mdsuite.calculators.flux_viscosity import GreenKuboViscosityFlux
 
 dict_classes_computations = {
     'EinsteinDiffusionCoefficients': EinsteinDiffusionCoefficients,
@@ -33,4 +34,5 @@ dict_classes_computations = {
     'StructureFactor': StructureFactor,
     'EinsteinHelfandThermalConductivity': EinsteinHelfandThermalConductivity,
     'GreenKuboViscosityFlux': GreenKuboViscosityFlux,
+    'GreenKuboViscosity': GreenKuboViscosity,
 }
