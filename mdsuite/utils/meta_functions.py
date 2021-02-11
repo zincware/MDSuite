f6cd4658f5996e829fa60ebedaf5f4972fa68494
SamTov <tovey.samuel@gmail.com>
2021-02-11 17:05:31 +0100

    Fix bug from previous merge
    Former-commit-id: ad133f64cc86d33927822430c03906bf965030f9

--- mdsuite/utils/meta_functions.py
+++ mdsuite/utils/meta_functions.py
@@ -361,4 +361,3 @@ def split_array(data: list, condition):
         return [data[condition]]
     else:                           # else return the whole array
         return initial_split
->>>>>>> 2451921298d258b92671e21320127e24916af511
