976e9687035892c640dbf2888c5fbaf7ba770331
PythonFZ <fabian.zills@web.de>
2021-02-15 17:30:46 +0100

    Update ADF Code

--- mdsuite/calculators/angular_distribution_function.py
+++ mdsuite/calculators/angular_distribution_function.py
@@ -7,33 +7,48 @@ from tqdm import tqdm
 
 from mdsuite.calculators.calculator import Calculator
 from mdsuite.utils.neighbour_list import get_neighbour_list, get_triu_indicies, get_triplets
-from mdsuite.utils.linalg import get_angles
+from mdsuite.utils.linalg import get_angles, angles_rij
 
 import matplotlib.pyplot as plt
 
 
 class AngularDistributionFunction(Calculator, ABC):
 
-    def __init__(self, obj, n_batches=1, n_confs=50, r_cut: int = 6.0, start: int = 0, stop: int = 10000,
-                 bins: int = 500):
+    def __init__(self, obj, n_batches=1, n_minibatches=50, n_confs=50, r_cut: int = 6.0, start: int = 0, stop: int = 10000,
+                 bins: int = 500, use_tf_function: bool = False):
         """Compute the Angular Distribution Function for all species combinations
 
         Parameters
         ----------
-        obj
-        n_batches
-        n_confs
+        n_batches : int
+            Number of batches, to split the configurations into.
+        n_minibatches: int
+            Number of minibatches for computing the angles to split each batch into
+        n_confs: int
+            Number of configurations to analyse
+        r_cut: float
+            cutoff radius for the ADF
+        start: int
+            Index of the first configuration
+        stop: int
+            Index of the last configuration
+        bins: int
+            bins for the ADF
+        use_tf_function: bool, default False
+            activate the tf.function decorator for the minibatches. Can speed up the calculation significantly, but
+            may lead to excessiv use of memory! During the first batch, this function will be traced. Tracing is slow,
+            so this might only be useful for a larger number of batches.
         """
-        # TODO update docstring!
         super().__init__(obj)
+        self.use_tf_function = use_tf_function
         self.r_cut = r_cut
         self.start = start
         self.stop = stop
         self.n_confs = n_confs
         self.bins = bins
-        self.bin_range = [0.0, 3.15]
+        self.bin_range = [0.0, 3.15]  # from 0 to pi
         self.n_batches = n_batches  # memory management for all batches
-        self.n_batches_triples = 100  # memory management for triples generation per batch.
+        self.n_minibatches = n_minibatches  # memory management for triples generation per batch.
 
     def _load_positions(self, indices):
         # TODO make parent class?
@@ -57,6 +72,16 @@ class AngularDistributionFunction(Calculator, ABC):
 
         angles = {}
 
+        if self.use_tf_function:
+            @tf.function
+            def _get_triplets(x):
+                return get_triplets(x, r_cut=self.r_cut,
+                                    n_atoms=self.parent.number_of_atoms, n_batches=self.n_minibatches)
+        else:
+            def _get_triplets(x):
+                return get_triplets(x, r_cut=self.r_cut,
+                                    n_atoms=self.parent.number_of_atoms, n_batches=self.n_minibatches)
+
         for idx in tqdm(np.array_split(sample_configs, self.n_batches), ncols=70):
             positions = self._load_positions(idx)
 
@@ -74,8 +99,7 @@ class AngularDistributionFunction(Calculator, ABC):
             r_ij_mat -= tf.transpose(r_ij_mat, (1, 0, 2, 3))
             r_ij_mat = tf.transpose(r_ij_mat, (3, 0, 1, 2))
 
-            r_ijk_indices = get_triplets(r_ij_mat, r_cut=self.r_cut, n_atoms=self.parent.number_of_atoms,
-                                         n_batches=self.n_batches_triples)
+            r_ijk_indices = _get_triplets(r_ij_mat)
 
             for species in itertools.combinations_with_replacement(species_indices, 3):
                 # Select the part of the r_ijk indices, where the selected species triple occurs.
@@ -108,10 +132,14 @@ class AngularDistributionFunction(Calculator, ABC):
         for species in itertools.combinations_with_replacement(species_indices, 3):
             name = f"{species[0][0]}-{species[1][0]}-{species[2][0]}"
             hist = angles.get(name)
+
+            bin_range_to_angles = np.linspace(self.bin_range[0] * (180 / 3.14159),
+                                              self.bin_range[1] * (180 / 3.14159),
+                                              self.bins)
+
             fig, ax = plt.subplots()
-            ax.plot(np.linspace(self.bin_range[0] * (180 / 3.14159), self.bin_range[1] * (180 / 3.14159), self.bins),
-                    hist)
-            ax.set_title(name)
+            ax.plot(bin_range_to_angles, hist)
+            ax.set_title(f"{name} - Max: {bin_range_to_angles[tf.math.argmax(hist)]:.3f}° ")
             fig.show()
-            # TODO fig.show() does not work?
-            fig.savefig(f"{self.parent.figures_path}/adf_{name}.png")
+            # fig.savefig(f"{self.parent.figures_path}/adf_{name}.png")
+            fig.savefig(fr"img/adf_{name}.png")
