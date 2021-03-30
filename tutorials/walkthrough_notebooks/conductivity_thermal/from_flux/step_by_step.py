3751c3dac18c0e4eb6618767d286ba558aed1c62
Fran <fratorhe@vki.ac.be>
2021-03-30 18:37:38 +0200

    changed the name on reading flux files. Now the calculator for flux files is the same as for trajectory GK. Deleted the conductivity from flux calculator file

--- tutorials/walkthrough_notebooks/conductivity_thermal/from_flux/step_by_step.py
+++ tutorials/walkthrough_notebooks/conductivity_thermal/from_flux/step_by_step.py
@@ -15,5 +15,6 @@ if __name__ == '__main__':
                              time_step=4,
                              units='real')
 
-    argon.add_data(trajectory_file='../flux_1.dat', file_format='lammps_flux', rename_cols={'Flux_Thermal':['c_flux[1]', 'c_flux[2]', 'c_flux[3]']})
-    argon.run_computation('GreenKuboThermalConductivityFlux', data_range=2000, plot=True, correlation_time=3)
+    # the rename cols is only used to show here how it works, but in this case, the lammps file has the same column names...
+    argon.add_data(trajectory_file='../flux_1.lmp_flux', file_format='lammps_flux', rename_cols={'Thermal_Flux':['c_flux_thermal[1]', 'c_flux_thermal[2]', 'c_flux_thermal[3]']})
+    argon.run_computation('GreenKuboThermalConductivity', data_range=2000, plot=True, correlation_time=3)
