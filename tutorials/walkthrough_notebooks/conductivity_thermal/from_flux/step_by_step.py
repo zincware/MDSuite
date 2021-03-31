a486b8867f1a64d843389632c8a2233313d4a1b3
Fran <fratorhe@vki.ac.be>
2021-03-31 14:37:55 +0200

    bugfix

--- tutorials/walkthrough_notebooks/conductivity_thermal/from_flux/step_by_step.py
+++ tutorials/walkthrough_notebooks/conductivity_thermal/from_flux/step_by_step.py
@@ -4,11 +4,14 @@ from mdsuite.experiment.experiment import Experiment
 
 if __name__ == '__main__':
 
-    try:
-        new_case = True
-        shutil.rmtree('Argon_70')
-    except FileNotFoundError:
-        pass
+    new_case = False
+
+    if new_case:
+        try:
+            shutil.rmtree('Argon_70')
+        except FileNotFoundError:
+            pass
+
     argon = Experiment(analysis_name="Argon_70",
                              storage_path=".",
                              temperature=70.0,
@@ -16,5 +19,6 @@ if __name__ == '__main__':
                              units='real')
 
     # the rename cols is only used to show here how it works, but in this case, the lammps file has the same column names...
-    argon.add_data(trajectory_file='../flux_1.lmp_flux', file_format='lammps_flux', rename_cols={'Thermal_Flux':['c_flux_thermal[1]', 'c_flux_thermal[2]', 'c_flux_thermal[3]']})
+    if new_case:
+        argon.add_data(trajectory_file='../flux_1.lmp_flux', file_format='lammps_flux', rename_cols={'Thermal_Flux':['c_flux_thermal[1]', 'c_flux_thermal[2]', 'c_flux_thermal[3]']})
     argon.run_computation('GreenKuboThermalConductivity', data_range=2000, plot=True, correlation_time=3)
