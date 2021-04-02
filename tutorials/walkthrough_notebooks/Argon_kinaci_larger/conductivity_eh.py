2db2327d42f8d38ab2ce532393e7ec4f72b3bb23
Fran <fratorhe@vki.ac.be>
2021-04-02 16:57:10 +0200

    test case from the paper of Kinaci (with less atoms, but should be enough, I hope)
    Former-commit-id: d2ea7223aa9a7a2d09f1f46b7c56dd223e097fcc

---
+++ tutorials/walkthrough_notebooks/Argon_kinaci_larger/conductivity_eh.py
@@ -0,0 +1,22 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import shutil
+
+import mdsuite as mds # Cool name for professionalism purposes
+
+new_case = False
+
+if new_case:
+    try:
+        shutil.rmtree('Argon')
+    except FileNotFoundError:
+        pass
+
+
+
+argon = mds.Experiment(analysis_name="Argon", time_step=2, temperature=50.0, units='real')
+
+if new_case:
+    argon.add_data(trajectory_file='../gk_data.lmp_traj')
+
+argon.run_computation('EinsteinHelfandThermalConductivity', data_range=15000, plot=True, correlation_time=2)
