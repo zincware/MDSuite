75273bcf3bea20dd83b309a97ad1cebca8f0d950
SamTov <tovey.samuel@gmail.com>
2021-04-10 15:02:10 +0200

    Add molecule id storage for molecule mapping
    Former-commit-id: 6b3212196de647528ff91f3c7ecaf75c26ae7da7

--- tutorials/scripts/map_molecules.py
+++ tutorials/scripts/map_molecules.py
@@ -2,6 +2,9 @@
 Example file for how to map atoms from a simulation to defined molecules.
 """
 
+import sys
+sys.path.insert(1, "../../")
+
 import mdsuite as mds
 from mdsuite.transformations.map_molecules import MolecularMap
 import shutil
@@ -21,12 +24,13 @@ def main():
     emim.species['C']['mass'] = 12.011
     emim.species['H']['mass'] = 1.008
     emim.species['F']['mass'] = 18.998
-    emim.run_computation.EinsteinHelfandIonicConductivity(plot=True, data_range=50)
-    emim.run_computation.GreenKuboIonicConductivity(plot=True, data_range=50)
-    #emim.perform_transformation("UnwrapCoordinates")
-    #mapper = MolecularMap(emim, molecules={'emim': {'smiles': 'CCN1C=C[N+](+C1)C', 'amount': 20}})
-    #mapper.run_transformation()
-    #emim.perform_transformation('WrapCoordinates', species=['emim'])
+    #emim.run_computation.EinsteinHelfandIonicConductivity(plot=True, data_range=50)
+    #emim.run_computation.GreenKuboIonicConductivity(plot=True, data_range=50)
+    emim.perform_transformation("UnwrapCoordinates")
+    mapper = MolecularMap(emim, molecules={'emim': {'smiles': 'CCN1C=C[N+](+C1)C', 'amount': 20}})
+    mapper.run_transformation()
+    emim.perform_transformation('WrapCoordinates', species=['emim'])
+    print(emim.molecules)
     #emim.write_xyz(species=['emim'], name='emim', dump_property='Positions')
     #emim.write_xyz(species=['C', 'H', 'N'], name='atomistic', dump_property='Positions')
 
