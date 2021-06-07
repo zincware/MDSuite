c72c70f32cf89fa45fe57da0c8a25076fbc75f8f
PythonFZ <fabian.zills@web.de>
2021-06-07 16:41:36 +0200

    Remove Tutorials

--- tutorials/walkthrough_notebooks/Liquid_Argon/from_traj/viscosity_gk.py
+++
@@ -1,8 +0,0 @@
-"""
-Compute viscosity from Green-Kubo
-"""
-import mdsuite as mds
-
-argon = mds.Experiment(analysis_name="Argon", time_step=2, temperature=94.4, units='real')
-argon.add_data(trajectory_file='../gk_data.lmp_traj')
-argon.run_computation('GreenKuboViscosity', data_range=19980, plot=True)
