f743afed46f4b70c4f8036d87fdbdf4138fc75e4
PythonFZ <fabian.zills@web.de>
2021-09-01 19:12:24 +0200

    update + tests

--- mdsuite/calculators/green_kubo_self_diffusion_coefficients.py
+++ mdsuite/calculators/green_kubo_self_diffusion_coefficients.py
@@ -55,7 +55,7 @@ class GreenKuboSelfDiffusionCoefficients(Calculator):
     experiment.run_computation.GreenKuboSelfDiffusionCoefficients(data_range=500, plot=True, correlation_time=10)
     """
 
-    def __init__(self, experiment):
+    def __init__(self, **kwargs):
         """
         Constructor for the Green Kubo diffusion coefficients class.
 
@@ -65,7 +65,7 @@ class GreenKuboSelfDiffusionCoefficients(Calculator):
                 Experiment class to call from
         """
 
-        super().__init__(experiment)
+        super().__init__(**kwargs)
 
         self.loaded_property = 'Velocities'  # Property to be loaded for the analysis
         self.scale_function = {'linear': {'scale_factor': 150}}
@@ -91,29 +91,44 @@ class GreenKuboSelfDiffusionCoefficients(Calculator):
                 Number of configurations to use in each ensemble
         save :
                 If true, tensor_values will be saved after the analysis
+
+        Returns
+        -------
+        data:
+            A dictionary of shape {experiment_name: data} for multiple len(experiments) > 1 or otherwise just data
+
         """
 
-        self.update_user_args(plot=plot, data_range=data_range, save=save, correlation_time=correlation_time,
-                              atom_selection=atom_selection, export=export, gpu=gpu)
+        out = {}
+        for experiment in self.experiments:
+            self.experiment = experiment
 
-        self.molecules = molecules
-        self.species = species  # Which species to calculate for
+            self.update_user_args(plot=plot, data_range=data_range, save=save, correlation_time=correlation_time,
+                                  atom_selection=atom_selection, export=export, gpu=gpu)
 
-        self.vacf = np.zeros(self.data_range)
-        self.sigma = []
+            self.molecules = molecules
+            self.species = species  # Which species to calculate for
 
-        if species is None:
-            if molecules:
-                self.species = list(self.experiment.molecules)
+            self.vacf = np.zeros(self.data_range)
+            self.sigma = []
+
+            if species is None:
+                if molecules:
+                    self.species = list(self.experiment.molecules)
+                else:
+                    self.species = list(self.experiment.species)
+
+            if self.load_data:
+                out[self.experiment.experiment_name] = self.experiment.export_property_data(
+                    {"Analysis": self.analysis_name}
+                )
             else:
-                self.species = list(self.experiment.species)
+                out[self.experiment.experiment_name] = self.run_analysis()
 
-        out = self.run_analysis()
-
-        self.experiment.save_class()
-        # need to move save_class() to here, because it can't be done in the experiment any more!
-
-        return out
+        if len(self.experiments) > 1:
+            return out
+        else:
+            return out[self.experiment.experiment_name]
 
     def _update_output_signatures(self):
         """
@@ -143,13 +158,13 @@ class GreenKuboSelfDiffusionCoefficients(Calculator):
             # Calculate the prefactor
             numerator = self.experiment.units['length'] ** 2
             denominator = 3 * self.experiment.units['time'] * (self.data_range - 1) * \
-                len(self.experiment.molecules[species]['indices'])
+                          len(self.experiment.molecules[species]['indices'])
             self.prefactor = numerator / denominator
         else:
             # Calculate the prefactor
             numerator = self.experiment.units['length'] ** 2
             denominator = 3 * self.experiment.units['time'] * (self.data_range - 1) * \
-                len(self.experiment.species[species]['indices'])
+                          len(self.experiment.species[species]['indices'])
             self.prefactor = numerator / denominator
 
     def _apply_averaging_factor(self):
