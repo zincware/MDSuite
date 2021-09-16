6527d202c9baa62c191d79bbab12c0d099907438
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2021-09-16 10:11:27 +0200

    doc, simplify

--- mdsuite/transformations/velocity_from_positions.py
+++ mdsuite/transformations/velocity_from_positions.py
@@ -7,11 +7,9 @@ SPDX-License-Identifier: EPL-2.0
 
 Copyright Contributors to the MDSuite Project.
 
-Atomic transformation to wrap the simulation coordinates.
-
 Summary
 -------
-Calculate the velocity of particles from their positions via simple v = delta x/ delta t
+Calculate the velocity of particles from their positions
 """
 import numpy as np
 import os
@@ -22,10 +20,25 @@ from mdsuite import experiment
 
 
 class VelocityFromPositions(Transformations):
+    """
+    Calculate the velocity based on the particle positions via simple forward derivative, i.e.
+    v(t) = (x(t+dt)-x(t))/dt. The last velocity of the trajectory cannot be computed and is copied
+    from the second to last.
+    """
 
     def __init__(self,
                  exp: experiment.Experiment,
                  species: list = None):
+        """
+        Standard constructor
+
+        Parameters
+        ----------
+        exp : mdsuite.experiment.Experiment
+            The experiment on which to perform the computation.
+        species : list, optional
+            A list of species for which to perform the computation. Default: All species.
+        """
         super().__init__(exp)
 
         self.scale_function = {'linear': {'scale_factor': 1}}
@@ -38,25 +51,28 @@ class VelocityFromPositions(Transformations):
         if species is None:
             self.species = list(self.experiment.species)
 
-    def _load_data(self, species):
-        path = join_path(species, "Unwrapped_Positions")
-        return tf.convert_to_tensor(self.experiment.load_matrix(path=[path], select_slice=np.s_[:]))
-
     def run_transformation(self):
+        """
+        Perform the velocity calculation.
+        """
         output_name = "Velocities_From_Positions"
         for species in self.species:
 
             exists = self.database.check_existence(os.path.join(species, output_name))
             # Check if the tensor_values has already been unwrapped
             if exists:
-                print(f"Velocities from Positions exists for {species}, "
+                print(f"{output_name} exists for {species}, "
                       f"using the saved coordinates")
             else:
-                pos = self._load_data(species)
-                pos_rolled = tf.roll(pos, axis=1, shift=1)
-                vel = (pos- pos_rolled)/ self.dt
-                # discard first value, it comes from wrapping around the positions
-                vel = vel[:, 1:, :]
+                pos = tf.convert_to_tensor(self.experiment.load_matrix(identifier="Unwrapped_Positions",
+                                                                       species=species,
+                                                                       select_slice=np.s_[:]))
+                pos_plus_dt = tf.roll(pos, axis=1, shift=-1)
+                vel = (pos_plus_dt - pos) / self.dt
+                # discard last value, it comes from wrapping around the positions
+                vel = vel[:, -1:, :]
+                # instead, append the last value again
+                vel = tf.concat(vel, vel[:, -1, :], axis=1)
 
                 path = join_path(species, output_name)
                 dataset_structure = {species: {output_name: tuple(np.shape(vel))}}
