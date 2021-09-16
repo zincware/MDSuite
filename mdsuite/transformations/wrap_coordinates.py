c21c3f7089833a75bd70442cc03d10f23f294e58
Christoph Lohrmann <clohrmann@icp.uni-stuttgart.de>
2021-09-16 10:32:57 +0200

    batch, doc

--- mdsuite/transformations/wrap_coordinates.py
+++ mdsuite/transformations/wrap_coordinates.py
@@ -17,13 +17,10 @@ positions. This module will do that for you.
 import numpy as np
 import os
 import tensorflow as tf
+import tqdm
 from mdsuite.transformations.transformations import Transformations
 from mdsuite.utils.meta_functions import join_path
 from mdsuite import experiment
-
-
-def shift_positions(pos: tf.Tensor, shift):
-    return pos + shift[None, None, :]
 
 
 class CoordinateWrapper(Transformations):
@@ -88,55 +85,95 @@ class CoordinateWrapper(Transformations):
         if species is None:
             self.species = list(self.experiment.species)
 
-    def _load_data(self, species, data_name: str):
-        # TODO this method should probably be a member of experiment. (building the path is not the job of the caller)
+    def _wrap_positions(self, unwrap_pos: tf.Tensor) -> tf.Tensor:
         """
-        Load the tensor_values to be unwrapped
-        """
-        path = join_path(species, data_name)
-        return tf.Tensor(self.experiment.load_matrix(path=[path], select_slice=np.s_[:]))
+        Do the actual wrapping of positions
 
-    def _wrap_positions(self, pos: tf.Tensor) -> tf.Tensor:
-        # add extra dimensions to box_l so the direction is clear
+        Parameters
+        ----------
+        unwrap_pos : tf.Tensor
+            The coordinates to wrap
+
+        Returns
+        -------
+        tf.Tensor containing the wrapped coordinates. Same shape as unwrap_pos.
+        """
+
         box_l = self.box_array[None, None, :]
-        return pos - tf.floor(pos / box_l) * box_l
+
+        if self.center_box:
+            unwrap_pos = unwrap_pos + box_l / 2.
+        pos = unwrap_pos - tf.floor(unwrap_pos / box_l) * box_l
+        if self.center_box:
+            pos = pos - box_l / 2.
+
+        return pos
+
+    def _prepare_database_entry(self, species: str):
+        """
+        Add the relevant datasets and groups in the database_path
+
+        Parameters
+        ----------
+        species : str
+                Species for which tensor_values will be added.
+        Returns
+        -------
+        tensor_values structure for use in saving the tensor_values to the database_path.
+        """
+
+        path = join_path(species, 'Positions')
+        existing = self._run_dataset_check(path)
+        if existing:
+            old_shape = self.database.get_data_size(path)
+            species_length = len(self.experiment.species[species]['indices'])
+            resize_structure = {path: (species_length, self.experiment.number_of_configurations - old_shape[0], 3)}
+            self.offset = old_shape[0]
+            self.database.resize_dataset(resize_structure)  # add a new dataset to the database_path
+            data_structure = {path: {'indices': np.s_[:], 'columns': [0, 1, 2], 'length': species_length}}
+        else:
+            species_length = len(self.experiment.species[species]['indices'])
+            number_of_configurations = self.experiment.number_of_configurations
+            dataset_structure = {path: (species_length, number_of_configurations, 3)}
+            self.database.add_dataset(dataset_structure)
+            data_structure = {path: {'indices': np.s_[:], 'columns': [0, 1, 2], 'length': species_length}}
+
+        return data_structure
 
     def run_transformation(self):
         """
-        Collect the methods in the class and unwrap the coordinates
+        Wrap coordinates batch by batch.
         """
 
         output_name = "Positions"
 
         for species in self.species:
-
-            exists = self.database.check_existence(os.path.join(species,output_name))
-            # Check if the tensor_values has already been unwrapped
+            exists = self.database.check_existence(os.path.join(species, output_name))
             if exists:
                 print(f"Wrapped positions exists for {species}, "
                       f"using the saved coordinates")
             else:
-                unwrap_pos = self._load_data(species, "Positions")
 
-                if self.center_box:
-                    unwrap_pos = shift_positions(unwrap_pos, self.box_array / 2.)
-                wrapped_pos = self._wrap_positions(unwrap_pos)
-                if self.center_box:
-                    wrapped_pos = shift_positions(wrapped_pos, -self.box_array / 2.)
-
-                path = join_path(species, output_name)
-                dataset_structure = {species: {output_name: tuple(np.shape(wrapped_pos))}}
-                self.database.add_dataset(dataset_structure)
-                data_structure = {path: {'indices': np.s_[:],
-                                         'columns': [0, 1, 2],
-                                         'length': len(wrapped_pos)}}
-                self._save_coordinates(data=wrapped_pos,
-                                       data_structure=data_structure,
-                                       index=0,
-                                       batch_size=np.shape(wrapped_pos)[1],
-                                       system_tensor=False,
-                                       tensor=True)
-
-        # TODO save class does not exist anymore. save somewhere else?
-        #self.experiment.memory_requirements = self.database.get_memory_information()
-        #self.experiment.save_class()
+                data_structure = self._prepare_database_entry(species)
+                data_path = [join_path(species, output_name)]
+                self._prepare_monitors(data_path)
+                batch_generator, batch_generator_args = self.data_manager.batch_generator()
+                data_set = tf.data.Dataset.from_generator(batch_generator,
+                                                          args=batch_generator_args,
+                                                          output_signature=tf.TensorSpec(
+                                                              shape=(None, self.batch_size, 3),
+                                                              dtype=tf.float64)
+                                                          )
+                data_set = data_set.prefetch(tf.data.experimental.AUTOTUNE)
+                for index, x in tqdm.tqdm(enumerate(data_set),
+                                          ncols=70,
+                                          desc=f'{species}: Wrapping Coordinates'):
+                    unwrap_pos = tf.convert_to_tensor(self.experiment.load_matrix(identifier="Unwrapped_Positions",
+                                                                                  species=species))
+                    wrapped_pos = self._wrap_positions(unwrap_pos)
+                    self._save_coordinates(data=wrapped_pos,
+                                           data_structure=data_structure,
+                                           index=index,
+                                           batch_size=self.batch_size,
+                                           system_tensor=False,
+                                           tensor=True)
