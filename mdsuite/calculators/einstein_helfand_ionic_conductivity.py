5a3cf9f254a5389b06f359ea89f819ebd3b3113e
PythonFZ <fabian.zills@web.de>
2021-09-22 16:26:51 +0200

    update to database

--- mdsuite/calculators/einstein_helfand_ionic_conductivity.py
+++ mdsuite/calculators/einstein_helfand_ionic_conductivity.py
@@ -23,7 +23,6 @@ from mdsuite.calculators.calculator import Calculator, call
 from mdsuite.utils.units import elementary_charge, boltzmann_constant
 from mdsuite.database.calculator_database import Parameters
 
-
 tqdm.monitor_interval = 0
 warnings.filterwarnings("ignore")
 
@@ -78,6 +77,9 @@ class EinsteinHelfandIonicConductivity(Calculator):
         self.analysis_name = 'Einstein Helfand Ionic Conductivity'
         self.prefactor: float
 
+        self.result_keys = ["ionic_conductivity", "uncertainty"]
+        self.result_series_keys = ["time", "msd"]
+
     @call
     def __call__(self, plot=True, data_range=500, save=True, correlation_time=1,
                  export: bool = False, gpu: bool = False):
@@ -104,6 +106,11 @@ class EinsteinHelfandIonicConductivity(Calculator):
         self.update_user_args(plot=plot, data_range=data_range, save=save, correlation_time=correlation_time,
                               export=export, gpu=gpu)
         self.msd_array = np.zeros(self.data_range)
+
+        return self.update_db_entry_with_kwargs(
+            data_range=data_range,
+            correlation_time=correlation_time
+        )
 
     def _update_output_signatures(self):
         """
@@ -174,18 +181,11 @@ class EinsteinHelfandIonicConductivity(Calculator):
             Analysis=self.analysis_name,
             data_range=self.data_range,
             data=[
-                {'ionic_conductivity': result[0], 'uncertainty': result[1]}],
+                {self.result_keys[0]: result[0], self.result_keys[1]: result[1]}],
             Subject=["System"]
         )
         data = properties.data
-        data += [{'time': x, 'msd': y} for x, y in
+        data += [{self.result_series_keys[0]: x, self.result_series_keys[1]: y} for x, y in
                  zip(self.time, self.msd_array)]
         properties.data = data
         self.update_database(properties)
-
-        # Update the plot if required
-        if self.plot:
-            self.run_visualization(
-                x_data=np.array(self.time) * self.experiment.units['time'],
-                y_data=self.msd_array * self.experiment.units['time'],
-                title=f'{result[0]:.3E} +- {result[1]:.3E}')
