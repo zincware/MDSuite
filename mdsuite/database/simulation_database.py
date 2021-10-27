46fdbe1d3b67cbd220ad58f3a5a8fc83a7b9fb72
Fabian <46721498+PythonFZ@users.noreply.github.com>
2021-10-27 10:47:20 +0200

    Update simulation_database.py

--- mdsuite/database/simulation_database.py
+++ mdsuite/database/simulation_database.py
@@ -385,7 +385,7 @@ class Database:
         structure : dict
                 General structure of the dictionary with relevant dataset sizes.
                 e.g. {'Na': {'Forces': (200, 5000, 3)}, 'Pressure': (5000, 6),
-                'Temperature': (5000, 1)} In this case, the last value in the tupl
+                'Temperature': (5000, 1)} In this case, the last value in the tuple
                 corresponds to the number of components that wil be parsed to the
                 database_path.
         Returns
