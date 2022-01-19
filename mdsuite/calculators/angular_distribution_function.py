993e735495b8bb0caeb416120390665aca6c223f
SamTov <tovey.samuel@gmail.com>
2022-01-19 12:05:54 +0100

    run black

--- mdsuite/calculators/angular_distribution_function.py
+++ mdsuite/calculators/angular_distribution_function.py
@@ -215,7 +215,9 @@ class AngularDistributionFunction(TrajectoryCalculator, ABC):
         self.gpu = gpu
         self.plot = plot
         self._batch_size = batch_size  # memory management for all batches
-        self.adf_minibatch = minibatch  # memory management for triples generation per batch.
+        self.adf_minibatch = (
+            minibatch  # memory management for triples generation per batch.
+        )
         self.bin_range = [0.0, 3.15]  # from 0 to a chemists pi
         self.norm_power = norm_power
         self.override_n_batches = kwargs.get("batches")
