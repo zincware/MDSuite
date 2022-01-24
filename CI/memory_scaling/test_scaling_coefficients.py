2d85c89e14dcca146e08ea28e5892311de47b9cb
SamTov <tovey.samuel@gmail.com>
2022-01-24 18:12:02 +0100

    start memory measurement modules.

---
+++ CI/memory_scaling/test_scaling_coefficients.py
@@ -0,0 +1,37 @@
+"""
+MDSuite: A Zincwarecode package.
+
+License
+-------
+This program and the accompanying materials are made available under the terms
+of the Eclipse Public License v2.0 which accompanies this distribution, and is
+available at https://www.eclipse.org/legal/epl-v20.html
+
+SPDX-License-Identifier: EPL-2.0
+
+Copyright Contributors to the Zincwarecode Project.
+
+Contact Information
+-------------------
+email: zincwarecode@gmail.com
+github: https://github.com/zincware
+web: https://zincwarecode.com/
+
+Citation
+--------
+If you use this module please cite us with:
+
+Summary
+-------
+Module to test scaling coefficients.
+"""
+import pytest
+
+import mdsuite
+
+
+@pytest.fixture()
+def mdsuite_project(tmp_path) -> mdsuite.Project:
+    project = mdsuite.Project(storage_path=tmp_path.as_posix())
+
+    return project
