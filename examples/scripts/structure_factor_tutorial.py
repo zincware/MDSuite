beb11ca67c2a02d12fc71a46522bb4bab53ca392
MarcBrueck <marco.brueckner97@web.de>
2022-01-28 10:01:33 +0100

    ran black on other file

--- examples/scripts/structure_factor_tutorial.py
+++ examples/scripts/structure_factor_tutorial.py
@@ -1,4 +1,3 @@
-
 import mdsuite as mds
 
 from mdsuite.calculators.structure_factor import StructureFactor
@@ -6,22 +5,20 @@ from mdsuite.calculators.structure_factor import StructureFactor
 
 project = mds.Project("struc_fac")
 project.add_experiment(
-	name="NaCl_1399K",
-	timestep=2.0,
-	temperature=1400.0,
-	units="metal",
-	simulation_data="short.lammpstraj",
+    name="NaCl_1399K",
+    timestep=2.0,
+    temperature=1400.0,
+    units="metal",
+    simulation_data="short.lammpstraj",
 )
 
-'''Make sure to give your particles the right charge,
-otherwise the calculated structure factor will be incorrect'''
-project.experiments['NaCl_1399K'].set_charge('Na',[+1])
-project.experiments['NaCl_1399K'].set_charge('Cl',[-1])
+"""Make sure to give your particles the right charge,
+otherwise the calculated structure factor will be incorrect"""
+project.experiments["NaCl_1399K"].set_charge("Na", [+1])
+project.experiments["NaCl_1399K"].set_charge("Cl", [-1])
 
 # You also need to use the correct mass, but this is usually already set
 # project.experiments['NaCl_1399K'].set_mass('Cl',[50])
 
 
 project.run.StructureFactor(plot=True)
-
-
