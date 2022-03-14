6b1f04973711e5924b7f9aca8416a7c92f0ff844
SamTov <tovey.samuel@gmail.com>
2022-03-14 10:46:30 -0500

    fix gk axis

--- mdsuite/calculators/__init__.py
+++ mdsuite/calculators/__init__.py
@@ -26,29 +26,44 @@ Summary
 """
 from __future__ import annotations
 
-from mdsuite.calculators.angular_distribution_function import AngularDistributionFunction
+from mdsuite.calculators.angular_distribution_function import (
+    AngularDistributionFunction,
+)
 from mdsuite.calculators.calculator import Calculator
 from mdsuite.calculators.coordination_number_calculation import CoordinationNumbers
-from mdsuite.calculators.einstein_diffusion_coefficients import EinsteinDiffusionCoefficients
+from mdsuite.calculators.einstein_diffusion_coefficients import (
+    EinsteinDiffusionCoefficients,
+)
 from mdsuite.calculators.einstein_distinct_diffusion_coefficients import (
     EinsteinDistinctDiffusionCoefficients,
 )
-from mdsuite.calculators.einstein_helfand_ionic_conductivity import EinsteinHelfandIonicConductivity
-from mdsuite.calculators.einstein_helfand_thermal_conductivity import EinsteinHelfandThermalConductivity
-from mdsuite.calculators.einstein_helfand_thermal_kinaci import EinsteinHelfandThermalKinaci
+from mdsuite.calculators.einstein_helfand_ionic_conductivity import (
+    EinsteinHelfandIonicConductivity,
+)
+from mdsuite.calculators.einstein_helfand_thermal_conductivity import (
+    EinsteinHelfandThermalConductivity,
+)
+from mdsuite.calculators.einstein_helfand_thermal_kinaci import (
+    EinsteinHelfandThermalKinaci,
+)
 from mdsuite.calculators.green_kubo_distinct_diffusion_coefficients import (
     GreenKuboDistinctDiffusionCoefficients,
 )
 from mdsuite.calculators.green_kubo_ionic_conductivity import GreenKuboIonicConductivity
-from mdsuite.calculators.green_kubo_self_diffusion_coefficients import GreenKuboDiffusionCoefficients
-from mdsuite.calculators.green_kubo_thermal_conductivity import GreenKuboThermalConductivity
+from mdsuite.calculators.green_kubo_self_diffusion_coefficients import (
+    GreenKuboDiffusionCoefficients,
+)
+from mdsuite.calculators.green_kubo_thermal_conductivity import (
+    GreenKuboThermalConductivity,
+)
 from mdsuite.calculators.green_kubo_viscosity import GreenKuboViscosity
 from mdsuite.calculators.green_kubo_viscosity_flux import GreenKuboViscosityFlux
 from mdsuite.calculators.kirkwood_buff_integrals import KirkwoodBuffIntegral
-from mdsuite.calculators.nernst_einstein_ionic_conductivity import NernstEinsteinIonicConductivity
+from mdsuite.calculators.nernst_einstein_ionic_conductivity import (
+    NernstEinsteinIonicConductivity,
+)
 from mdsuite.calculators.potential_of_mean_force import PotentialOfMeanForce
 from mdsuite.calculators.radial_distribution_function import RadialDistributionFunction
-
 
 __all__ = [
     Calculator.__name__,
