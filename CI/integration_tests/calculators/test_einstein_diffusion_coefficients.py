Sam tov fix memory scaling tests (#564)
* bad run to see CI output

* More work

* move concat to stack

* correct einstein calculator

* update tests and add test for final data range

* remove memory reduction.

* remove duplicate import

CI/integration_tests/calculators/test_einstein_diffusion_coefficients.py
@@ -51,7 +51,7 @@ def test_calculator(tmp_path):
 
     n_part = 500
     n_step = 5000
-    msd_range = 500
+    msd_range = 200
 
     vel = np.sqrt(2 * diff_coeff / time_step) * tf.random.normal(
         shape=(n_step, n_part, 3), mean=0, stddev=1