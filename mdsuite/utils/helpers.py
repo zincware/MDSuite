55060e60f906958a35ea29fcb0c31898bc6d35f7
PythonFZ <fabian.zills@web.de>
2023-01-24 17:22:49 +0100

    update D405

--- mdsuite/utils/helpers.py
+++ mdsuite/utils/helpers.py
@@ -1,4 +1,4 @@
-"""MDSuite helpers
+"""MDSuite helpers.
 
 This program and the accompanying materials are made available under the terms of the
 Eclipse Public License v2.0 which accompanies this distribution, and is available at
