b98df91916089e721c5188d2e055d2296557cdad
PythonFZ <fabian.zills@web.de>
2023-01-31 16:58:29 +0100

    use `mds.utils.DatasetKeys.OBSERVABLES`

--- mdsuite/calculators/green_kubo_viscosity.py
+++ mdsuite/calculators/green_kubo_viscosity.py
@@ -38,6 +38,7 @@ from tqdm import tqdm
 from mdsuite.calculators.calculator import call
 from mdsuite.calculators.trajectory_calculator import TrajectoryCalculator
 from mdsuite.database.mdsuite_properties import mdsuite_properties
+from mdsuite.utils import DatasetKeys
 
 
 @dataclass
@@ -252,9 +253,11 @@ class GreenKuboViscosity(TrajectoryCalculator, ABC):
         # Compute the pre-factor early.
         self._calculate_prefactor()
 
-        dict_ref = str.encode("/".join(["Observables", self.loaded_property.name]))
+        dict_ref = str.encode(
+            "/".join([DatasetKeys.OBSERVABLES, self.loaded_property.name])
+        )
 
-        batch_ds = self.get_batch_dataset(["Observables"])
+        batch_ds = self.get_batch_dataset([DatasetKeys.OBSERVABLES])
 
         for batch in tqdm(
             batch_ds,
@@ -262,7 +265,7 @@ class GreenKuboViscosity(TrajectoryCalculator, ABC):
             total=self.n_batches,
             disable=self.memory_manager.minibatch,
         ):
-            ensemble_ds = self.get_ensemble_dataset(batch, "Observables")
+            ensemble_ds = self.get_ensemble_dataset(batch, DatasetKeys.OBSERVABLES)
 
             for ensemble in ensemble_ds:
                 self.ensemble_operation(ensemble[dict_ref])
